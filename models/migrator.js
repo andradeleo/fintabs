1a2fcfc8d2ec1960af9313313a700265fd6f4296
Leonardo <contato.leoo0@gmail.com>
2025-02-11 01:01:47 -0300

    feat: create migrator model

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+const defaultMigrationsOption = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pg-migrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsOption,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrateMigrations = await migrationRunner({
+      ...defaultMigrationsOption,
+      dbClient,
+      dryRun: false,
+    });
+    return migrateMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
