c6e58611bcf10781983ad0864564394a0c42cd28
Leonardo <contato.leoo0@gmail.com>
2025-02-19 20:43:13 -0300

    feat: hide migrator logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationsOption = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pg-migrations",
 };
 
