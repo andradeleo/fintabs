6c328c9d4aa7c4f6345af888a0e0ac85bc9969c3
Leonardo <contato.leoo0@gmail.com>
2025-11-30 21:58:52 -0300

    feat: require read:actiovation_token to access /api/v1/activations/[token_id]

--- infra/controller.js
+++ infra/controller.js
@@ -17,7 +17,6 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  console.log({ error });
   if (
     error instanceof ValidationError ||
     error instanceof NotFoundError ||
@@ -61,7 +60,6 @@ async function clearSessionCookie(response) {
 }
 
 async function injectAnonymousOrUser(request, response, next) {
-  console.log("first middle");
   if (request.cookies?.session_id) {
     await injectAuthenticateUser(request);
     return next();
@@ -94,7 +92,6 @@ async function injectAnonymousUser(request) {
 }
 
 function canRequest(feature) {
-  console.log("can request");
   return function canRequestMiddleware(request, response, next) {
     const userTryingToRequest = request.context.user;
 
--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -12,7 +12,6 @@ router.get(controller.canRequest("read:session"), getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  console.log({ request });
   const sessionToken = request.cookies.session_id;
 
   const sessionObject = await session.findOneValidByToken(sessionToken);
